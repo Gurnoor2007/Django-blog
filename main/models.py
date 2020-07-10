3a89e8dd3fc67c04eb9d5cc1a91ed3c4d840e11b
Gurnoor <gurnoorsinghsaini07@gmail.com>
2020-07-10 15:12:50 +0530

    Created Contact Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact (models.Model):
+    contact_id = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=40)
+    topic = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
