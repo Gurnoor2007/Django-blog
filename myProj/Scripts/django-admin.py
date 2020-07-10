d765defbd11d68d665e62f846feb9402818ed4bc
Gurnoor <gurnoorsinghsaini07@gmail.com>
2020-07-10 13:59:39 +0530

    added blog app and installed django in env

---
+++ myProj/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Gurnoor singh\Programming\Python\Django Tutorial\Blog\MyBlog\myProj\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
